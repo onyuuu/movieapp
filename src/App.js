68b032a0d1f1201aee23497db91e842eeeb1a139
onyuuu <121140435+onyuuu@users.noreply.github.com>
2023-02-21 17:45:10 +0900

    home 수정

--- src/App.js
+++ src/App.js
@@ -1,26 +1,26 @@
-import './App.css';
-import React from 'react';
-import Navbar from './component/Navbar';
-import { Routes, Route } from 'react-router-dom';
-import Home from './pages/Home';
-import Movies from './pages/Movies';
-import Users from './pages/Users';
-import Footer from './component/Footer';
+import "./App.css";
+import React from "react";
+import Navbar from "./component/Navbar";
+import { Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import Movies from "./pages/Movies";
+import Users from "./pages/Users";
+import Footer from "./component/Footer";
 
 function App() {
+  return (
+    <div className="App">
+      <Navbar />
+      <Routes>
+        <Route path="/App" element={<Home />} />
+        <Route path="/" element={<Home />} />
+        <Route path="movies" element={<Movies />} />
+        <Route path="users" element={<Users />} />
+      </Routes>
 
-	return (
-		<div className="App">
-			<Navbar />
-			<Routes>
-				<Route path="/" element={<Home />} />
-				<Route path="movies" element={<Movies />} />
-				<Route path="users" element={<Users />} />
-			</Routes>
-
-			<Footer />
-		</div>
-	)
+      <Footer />
+    </div>
+  );
 }
 
 export default App;
